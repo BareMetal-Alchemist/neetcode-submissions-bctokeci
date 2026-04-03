c54844c1d07ec943e7a6fe59b4f16e820741966c
Noah Scott <149430676+BareMetal-Alchemist@users.noreply.github.com>
2026-04-02 20:43:13 -0700

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+
+        for(int i = 0; i < abs(n); i++)  ans *= x;
+        cout << ans;
+        if(n < 0) {
+            ans = 1/ans;
+        }
+        return ans;
+    }
+};
